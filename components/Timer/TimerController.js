3f3b6aa240599a95555be2f6b53a3e936a320c42
João Gonçalves <jsvgoncalves@gmail.com>
2016-04-12 18:10:09 +0200

    global: refactor Timer and User ctrl es6 classes
    * Refactors the two Controllers into classes ES6 style.
    
    Note: Some unextracted methods in the TimerController depend (and
    change) the $scope, so it's best to define them locally with let.
    
    github: updates #1
    
    Signed-off-by: Joao Goncalves <jsvgoncalves@gmail.com>

--- components/Timer/TimerController.js
+++ components/Timer/TimerController.js
@@ -2,43 +2,20 @@ import {checkBrowswerNotifications} from './../common/BrowserNotifications.js'
 import {randomSuggestion} from './../common/BrowserNotifications.js'
 import {prettyDate} from './../common/BrowserNotifications.js'
 
-export const TimerController = [
-	'$scope',
-	'$interval',
-	'$filter',
-	'$stateParams',
-	'PomodoroService',
-	'pomodoroList',
-	function($scope, $interval, $filter, $stateParams, PomodoroService, pomodoroList) {
+
+class TimerController {
+
+	constructor($scope, $interval, $filter, $stateParams, PomodoroService, pomodoroList) {
+		// The $interval
 		var timer = null
+		// Elapsed time
 		$scope.time = 0
+		// Flags
 		$scope.running = false
 		$scope.finished = false
-		// Set from cfg
+		// Set from configs loaded from API
 		$scope.maxTime = $stateParams.time
 		$scope.list = pomodoroList
-
-		console.log($scope.list)
-
-		let sendNotification = () => {
-			if(checkBrowswerNotifications()) {
-				const options = {
-					body: `You worked for ${prettyDate($scope.time)}!\n ${randomSuggestion()}`,
-					icon: 'img/pomodoro.png'
-				}
-				new Notification("Time for a break!", options);
-			}
-		}
-
-		let handleResponse = (response) => {
-			// response from the server
-			console.log(response)
-		}
-
-		let handleError = (result) => {
-			// handle errors
-			console.log(result)
-		}
 
 		/**
 		 * Updates the timer at each tick
@@ -47,9 +24,10 @@ export const TimerController = [
 		let updateTimer = () => {
 			$scope.time++;
 			if ($scope.time >= $scope.maxTime) {
-				sendNotification()
+				this.sendNotification($scope)
 				$scope.toggleTimer()
-				PomodoroService.savePomodoro({'params': '1'}).then(handleResponse, handleError)
+				PomodoroService.savePomodoro({'params': '1'})
+							   .then(this.handleResponse, this.handleError)
 			}
 		}
 
@@ -71,5 +49,36 @@ export const TimerController = [
 				$scope.running = true
 				timer = $interval(updateTimer, 1000)
 			}
+		}
 	}
-}]
+
+	/**
+	 * Save Pomodoro Successful
+	 */
+	handleResponse (response){
+		// response from the server
+		console.log(response)
+	}
+
+	/**
+	 * Save Pomodoro failed
+	 */
+	handleError (result){
+		console.log(result)
+	}
+
+	/**
+	 * Sends a browser notification with the Pomodoro results
+	 */
+	sendNotification(scope) {
+		if(checkBrowswerNotifications()) {
+			const options = {
+				body: `You worked for ${prettyDate(scope.time)}!\n ${randomSuggestion()}`,
+				icon: 'img/pomodoro.png'
+			}
+			new Notification("Time for a break!", options);
+		}
+	}
+}
+
+export { TimerController }
--- components/User/UserController.js
+++ components/User/UserController.js
@@ -1,15 +1,23 @@
-export const UserController = ['$scope', function($scope) {
-    // Input from server
-    $scope.user = {
-        'name' : 'Joao',
-        'email': 'jsvgoncalves@gmail.com'}
+class UserController {
 
-    // Input from server
-    $scope.sessions = [
-        {'start': '10-04-2016 20:00',
-         'finish': '10-04-2016 20:00'},
-         {'start': '10-04-2016 21:00',
-          'finish': '10-04-2016 22:00'}
-    ];
+	constructor($scope) {
+		//!TODO: Replace with service
+		$scope.user = {
+			'name' : 'Joao',
+			'email': 'jsvgoncalves@gmail.com'
+		};
+		//!TODO: Replace with service
+		$scope.sessions = [
+			{
+				'start': '10-04-2016 20:00',
+				'finish': '10-04-2016 20:00'
+			},
+			{
+				'start': '10-04-2016 21:00',
+				'finish': '10-04-2016 22:00'
+			}
+		];
+	}
+}
 
-}]
+export { UserController }
